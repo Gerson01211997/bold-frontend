dadb0f700e0d9d8510b49736df9a577ebbece403
Gerson Hoyos <gersonhoyos_11@hotmail.com>
2025-12-15 12:27:05 -0500

    :construction: save changes

--- src/modules/listTransactions/components/search/style.ts
+++ src/modules/listTransactions/components/search/style.ts
@@ -1,5 +1,5 @@
 export const className = {
   container: "relative flex items-center w-full bg-white p-3 gap-4",
   input:
-    "text-bold-gray placeholder-bold-gray bg-transparent border-none focus:outline-none text-base p-0",
+    "w-full text-bold-gray placeholder-bold-gray bg-transparent border-none focus:outline-none text-base p-0",
 } as const;
